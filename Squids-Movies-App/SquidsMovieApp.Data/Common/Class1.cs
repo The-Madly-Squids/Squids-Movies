42e6f9449eeea3b6733d53282e0ec0e3f5562966
Lubomir Yonchev <infra1515@gmail.com>
2018-03-13 11:13:43 +0200

    Initial commit. Buildied project skeleton.

---
+++ Squids-Movies-App/SquidsMovieApp.Data/Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquidsMovieApp.Data.Common
+{
+    class Class1
+    {
+    }
+}
