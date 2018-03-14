a5913f8e6893694b843ed335fe51cf738dcfa1c6
Lubomir Yonchev <infra1515@gmail.com>
2018-03-14 17:10:14 +0200

    added context placeholder and installed entity

---
+++ Squids-Movies-App/SquidsMovieApp.Data/Context/DBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquidsMovieApp.Data.Context
+{
+    public class DBContext : DbContext
+    {
+
+    }
+}
