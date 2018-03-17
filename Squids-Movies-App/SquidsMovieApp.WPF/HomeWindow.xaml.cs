7f3256960d9063f5ce5a8eb2b43506a74cbcd872
Plamen Atanasov <plamen.atanasov91@gmail.com>
2018-03-17 14:12:16 +0200

    Added WPF/UI. Added login and profile basic pages.

---
+++ Squids-Movies-App/SquidsMovieApp.WPF/HomeWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SquidsMovieApp.WPF
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class HomeWindow : Page
+    {
+        public HomeWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void LoginBtnClicked(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new ProfileWindow());
+        }
+
+        private void ExitBtnClicked(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
