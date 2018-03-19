06c1243418406bd3b8c094266e2cc7d94f08de0e
Anton Simeonov <djnoreaga@gmail.com>
2018-03-19 15:53:35 +0200

    working on user

---
+++ Squids-Movies-App/SquidsMovieApp.Core/Factories/UserModelFactory.cs
@@ -0,0 +1,29 @@
+﻿using SquidsMovieApp.Core.Factories.Contracts;
+using SquidsMovieApp.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquidsMovieApp.Core.Factories
+{
+    public class UserModelFactory : IUserFactory
+    {
+        public UserModel CreateUserModel(string firstName, string lastName, int? age,
+            string nickName, string email, string password, bool isAdmin, int moneyBalance)
+        {
+            return new UserModel()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Age = age,
+                Nickname = nickName,
+                Email = email,
+                Password = password,
+                IsAdmin = isAdmin,
+                MoneyBalance = moneyBalance
+            };
+        }
+    }
+}
---
+++ Squids-Movies-App/SquidsMovieApp.Logic/Contracts/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SquidsMovieApp.DTO;
+
+namespace SquidsMovieApp.Logic.Contracts
+{
+    public interface IUserService
+    {
+        IEnumerable<UserModel> GetAllUsers();
+        void AddUser(UserModel user);
+        void RemoveUser(UserModel user);
+
+    }
+}
