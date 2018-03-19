89b780d27f33417010505f313adad0747e1e11c4
Lubomir Yonchev <infra1515@gmail.com>
2018-03-19 12:28:28 +0200

    finished AddMovieParticipant. Added RoleService.

---
+++ Squids-Movies-App/SquidsMovieApp.Logic/RoleService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using SquidsMovieApp.Data.Context;
+using SquidsMovieApp.Data.Models;
+using SquidsMovieApp.DTO;
+using SquidsMovieApp.Logic.Contracts;
+
+namespace SquidsMovieApp.Logic
+{
+    public class RoleService : IRoleService
+    {
+        private readonly IMovieAppDBContext movieAppDbContext;
+        private readonly IMapper mapper;
+
+        public RoleService(IMovieAppDBContext movieAppDbContext, IMapper mapper)
+        {
+            this.movieAppDbContext = movieAppDbContext;
+            this.mapper = mapper;
+        }
+
+        public void AddRole(MovieModel movie, ParticipantModel participant, string roleName)
+        {
+            // use a factory?
+            var actorRole = new Role()
+            {
+                MovieId = movie.MovieId,
+                ParticipantId = participant.ParticipantId,
+                RoleName = roleName
+            };
+            this.movieAppDbContext.Roles.Add(actorRole);
+        }
+
+        public IEnumerable<Role> ParticipantRolesPerMovie(ParticipantModel participant,
+            MovieModel movie)
+        {
+            var pRoles = this.movieAppDbContext.Roles
+                .Where(x => x.ParticipantId == participant.ParticipantId &&
+                       x.MovieId == movie.MovieId);
+
+            return pRoles;
+        }
+    }
+}
