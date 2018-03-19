fc99e05ab66c7269d5833872fc8523beff7bfe31
Lubomir Yonchev <infra1515@gmail.com>
2018-03-19 18:22:37 +0200

    created ParticipantService and Controller.

---
+++ Squids-Movies-App/SquidsMovieApp.Logic/ParticipantService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SquidsMovieApp.Logic.Contracts;
+
+namespace SquidsMovieApp.Logic
+{
+    public class ParticipantService : IParticipantService
+    {
+
+    }
+}
