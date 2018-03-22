cc9e6514665d7655bb12bbcbffd7c5b07b698952
Plamen Atanasov <plamen.atanasov91@gmail.com>
2018-03-22 15:17:31 +0200

    Remvoved unneeded code from program.cs

--- Squids-Movies-App/SquidsMovieApp.Program/Program.cs
+++ Squids-Movies-App/SquidsMovieApp.Program/Program.cs
@@ -8,15 +8,6 @@ namespace SquidsMovieApp.Program
     {
         static void Main()
         {
-            string moviesPath = @"..\..\..\SquidsMovieApp.Utilities\JsonData\movies.json";
-            var parser = new Parser();
-            var webConverter = new WebConverter();
-            var movies = parser.ParseMovies(moviesPath);
-
-
-            var ctx = new MovieAppDBContext();
-            var converter = new MovieConverter(ctx, webConverter, parser);
-            converter.AddOrUpdateMovies(movies);
         }
     }
 }
